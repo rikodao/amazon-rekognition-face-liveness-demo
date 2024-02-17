78647589447fd430c444f5c6097e2801832b65b3
Oiso <naotoiso@amazon.co.jp>
2024-02-17 18:54:31 +0900

    crop face frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,11 +5,14 @@ import { ThemeProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import FaceLiveness from './Components/FaceLiveness';
 import ReferenceImage from './Components/ReferenceImage';
+import ImageUploader from './Components/ImageUploader';
 import {
   View,
   Flex,
+  Tabs,
 } from '@aws-amplify/ui-react';
-
+import { StorageManager } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
 import awsexports from './aws-exports';
 
 Amplify.configure(awsexports);
@@ -46,10 +49,17 @@ function App() {
           width="740px"
           maxWidth="740px"
         >
-          {faceLivenessAnalysis && faceLivenessAnalysis.Confidence ? (
-            <ReferenceImage faceLivenessAnalysis={faceLivenessAnalysis} tryagain={tryagain}></ReferenceImage>
-          ) :
-            (<FaceLiveness faceLivenessAnalysis={getfaceLivenessAnalysis} />)}
+           <Tabs
+    defaultValue={'Tab 1'}
+    items={[
+      { label: '本人確認書類アップロード', value: 'Tab 1', content: (<ImageUploader />) },
+      { label: 'FaceLiveness', value: 'Tab 2', content: (<>{faceLivenessAnalysis && faceLivenessAnalysis.Confidence ? (
+        <ReferenceImage faceLivenessAnalysis={faceLivenessAnalysis} tryagain={tryagain}></ReferenceImage>
+      ) :
+        (<FaceLiveness faceLivenessAnalysis={getfaceLivenessAnalysis} />)}</>) },
+    ]}
+  />
+          
 
         </View>
       </Flex>
---
+++ src/frontend/src/Components/ImageUploader.js
@@ -0,0 +1,104 @@
+import { useRef, useState, useCallback } from "react";
+import Webcam from "react-webcam";
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+};
+
+
+
+export default () => {
+    const webcamRef = useRef(null);
+    const [image, setImage] = useState(null);
+    const [presignedURL, setPresignedURL] = useState(null);
+    const endpoint = process.env.REACT_APP_ENV_API_URL ? process.env.REACT_APP_ENV_API_URL : ''
+
+    const getPresignedUrl = async () => {
+        const response = await fetch(endpoint + 'uploadsignedurl');
+        const data = await response.json();
+        console.log("------ uploadsignedurl response ------");
+        console.log(data)
+        
+        const url = data.body
+        setPresignedURL(url)
+    }
+
+    /*
+   * Get the Face Liveness Session Result
+   */
+    const handleUploadImagetoS3 = async (presignedURL, imageSrc) => {
+        /*
+         * API call to get the Face Liveness Session result
+         */
+        const res = await fetch(imageSrc)
+        const blob = await res.blob()
+
+        const response = await fetch(presignedURL, {
+            method: 'PUT',
+            body: blob,
+            headers: {
+                'Content-Type': 'image/jpeg',
+            }
+        })
+
+        if (!response.ok) {
+            alert("エラーが発生しました")
+            console.error("Erorr")
+            console.error(response)
+            return {
+                body: null
+            };
+        }
+        alert("アップロード完了");
+    };
+
+    const capture = useCallback(async () => {
+        const imageSrc = webcamRef.current?.getScreenshot();
+        if (imageSrc) {
+            setImage(imageSrc);
+            getPresignedUrl()
+        }        
+    }, [webcamRef]);
+
+    return (
+        <>
+            <header>
+                <h1>カメラアプリ</h1>
+            </header>
+
+            {(
+                <>
+                    <div>
+                        <Webcam
+                            audio={false}
+                            width={540}
+                            height={360}
+                            ref={webcamRef}
+                            screenshotFormat="image/jpeg"
+                            videoConstraints={videoConstraints}
+                        />
+                    </div>
+                    <button onClick={capture}>キャプチャ</button>
+                </>
+            )}
+            {image && (
+                <>
+                    <div>
+                        <img src={image} alt="Screenshot" />
+                    </div>
+
+                    <div>
+                        <button
+                            onClick={() => {
+                                handleUploadImagetoS3(presignedURL, image)
+                            }}
+                        >
+                            送信
+                        </button>
+                    </div>
+                </>
+            )}
+        </>
+    );
+};
